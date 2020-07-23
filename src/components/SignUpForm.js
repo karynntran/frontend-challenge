d0b70438d4394694335ef22dc715490c727e900b
Karynn Tran <karynntran@Karynns-MacBook-Pro.local>
2020-07-22 21:24:13 -0400

    validation functionality successfully working

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -1,61 +1,107 @@
 import React from 'react';
 import { validateForm, submitForm } from '../helperFunctions';
- 
+import '../styles/signupform.scss';
+
 class SignUpForm extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-    	firstname: null,
-    	lastname: null,
-    	email: null,
-    	company: null,
-    	role: null
+    this.state = { 
+    	formValues: {
+	    	firstname: "",
+	    	lastname: "",
+	    	email: "",
+	    	company: "",
+	    	role: ""
+    	},
+    	errors: {
+    		firstname: "",
+	    	lastname: "",
+	    	email: "",
+	    	company: "",
+	    	role: ""
+    	}
     };
   }
 
   handleChange = e => {
-  	let name = e.target.name;
-    this.setState({
-    	[name]: e.target.value}
-    );
+  	let { name, value } = e.target;
+	this.setState(prevState => ({
+	    formValues: {
+	        ...prevState.formValues,
+	        [name]: value
+	    }
+	}))
+  }
+
+  handleErrors = (errorType) => {
+	if (this.state.errors[errorType]) {
+		return <span className="error">{this.state.errors[errorType]}</span>
+	}
   }
 
   handleSubmit = e => {
-  	validateForm();
-    alert('A name was submitted: ' + this.state.name);
     e.preventDefault();
+    console.log(this.state.formValues)
+  	let errorsList = validateForm(this.state.formValues);
+   	this.setState({
+	    errors: errorsList
+	})
+  }
+
+  onFocus = e => {
+  	console.log('hit')
+  	let {name} = e.target;
+	this.setState(prevState => ({
+	    errors: {
+	        ...prevState.errors,
+	        [name]: ''
+	    }
+	}))
   }
 
   render() {
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          First Name:
-          <input name="firstname" type="text" value={this.state.value} onChange={this.handleChange} />
-        </label>
-        <label>
-          Last Name:
-          <input name="lastname" type="text" value={this.state.value} onChange={this.handleChange} />
-        </label>
-        <label>
-          Email:
-          <input name="email" type="email" value={this.state.value} onChange={this.handleChange} />
-        </label>
-        <label>
-          Company:
-          <input name="company" type="text" value={this.state.value} onChange={this.handleChange} />
-        </label>
-        <label>
-          Your Role:
-          <select name="role" onChange={this.handleChange}>
-          	<option>Investor</option>
-          	<option>Mentor</option>
-          	<option>Founder</option>
-          	<option>Network Member</option>
-          </select>
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
+    	<div id="signup-form">
+    	<form onSubmit={this.handleSubmit}>
+	    	<div className="input-group-name">
+		    	<label>
+		    		First Name:
+		    		<input onFocus={this.onFocus} name="firstname" type="text" value={this.state.value} onChange={this.handleChange} />
+		    		{this.handleErrors("firstname")}
+		    	</label>
+		    	<label>
+			    	Last Name:
+			    	<input onFocus={this.onFocus} name="lastname" type="text" value={this.state.value} onChange={this.handleChange} />
+		    		{this.handleErrors("lastname")}
+		    	</label>
+	    	</div>
+	    	<label>
+		    	Email:
+		    	<input onFocus={this.onFocus} name="email" type="email" value={this.state.value} onChange={this.handleChange} />
+	    		{this.handleErrors("email")}
+
+	    	</label>
+	    	<label>
+		    	Company:
+		    	<input onFocus={this.onFocus} name="company" type="text" value={this.state.value} onChange={this.handleChange} />
+	    		{this.handleErrors("email")}
+	    	</label>
+	    	<label>
+		    	Your Role:
+		    	<select onFocus={this.onFocus} name="role" onChange={this.handleChange}>
+		    		<option value="">Select your role</option>    
+			    	<option value="investor">Investor</option>
+			    	<option value="mentor">Mentor</option>
+			    	<option value="founder">Founder</option>
+			    	<option value="member">Network Member</option>
+	    		</select>
+	    		{this.handleErrors("role")}
+	    	</label>
+	    	<input type="submit" value="Submit" />
+    	</form>
+
+
+    	</div>
     );
   }
 }
--- src/helperFunctions/validateForm.js
+++ src/helperFunctions/validateForm.js
@@ -1,6 +1,33 @@
 // use this function to validate your form
 
-export default (body) => {
-  alert("test helper function")
-  return console.log(body)
+export default (formValues) => {
+	let errors = {};
+	for (let value in formValues) {
+		if (formValues[value] == "") {
+			switch (value) {
+				case 'firstname':
+					errors[value] = "First name is required."
+					break;
+				case 'lastname':
+					errors[value] = "Last name is required."
+					break;
+				case 'email':
+					errors[value] = "Email name is required."
+					break;
+				case 'company':
+					errors[value] = "Company name is required."
+					break;
+				case 'role':
+					errors[value] = "Please select your role."
+					break;
+			}
+		}
+	}
+
+	console.log(errors)
+	if (Object.keys(errors).length === 0) {
+		return 'valid';
+	}
+
+	return errors;
 }
