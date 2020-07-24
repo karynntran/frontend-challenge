69e9c13901b6dd46beb74fe147c715e3b8a7aea9
Karynn Tran <karynntran@Karynns-MacBook-Pro.local>
2020-07-23 22:59:20 -0400

    update q and a with brand guideline accents

--- src/components/Info.js
+++ src/components/Info.js
@@ -8,11 +8,11 @@ const Info = () => {
 			<div className="Info-content">
 				<h1>Pa<span className="copy-green">(i)</span>rent Programming</h1>
 				<div className="Info-question">
-					<div className="Info-qa-symbol">Q </div> 
+					<div className="Info-qa-symbol">Question </div> 
 					<div className="Info-question-answer">Why did the child component have such great self-esteem?</div>
 				</div>
 				<div className="Info-answer">
-					<div className="Info-qa-symbol">A </div>
+					<div className="Info-qa-symbol">Answer </div>
 					<div className="Info-question-answer">Because its parent kept giving it props!</div>
 				</div>
 				<p className="Info-details">If you like dad/mom jokes like this one, you'll have a blast at our upcoming event, <b>Pa(i)rent Programming</b>. Parents and parent-figures in tech and their kids are invited to come together and go through some fun paired coding exercises and enjoy some fun swag and refreshments.</p>
